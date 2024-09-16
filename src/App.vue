08343c46d2c3551bdb34d12cfd1952af54918843
mayrars <mayra203@gmail.com>
2024-09-16 09:20:50 -0600

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link> | º 
+      <router-link to="/register">Register</router-link> 
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+
+</script>
