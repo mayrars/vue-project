a71c39d39922984ed48a0d677f6d4555c8710472
mayrars <mayra203@gmail.com>
2024-09-17 05:10:51 -0600

    create user store and import in another components

---
+++ src/stores/user.js
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+
+export const useUserStore = defineStore('userStore', {
+    state: () => ({
+        userData: 'ejemplo@test.com'
+    }),
+    getters:{
+        minuscula(state){
+            return state.userData.toLowerCase()
+        }
+    }
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-        <h1>Home</h1>
+        <h1>Home - {{ userStore.userData }}</h1>
     </div>
 </template>
 
 <script setup>
-
+    import {useUserStore} from '../stores/user'
+    const userStore = useUserStore()
 </script>
