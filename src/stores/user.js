f62a90b00c152647f8d35b0568fbbdb7e8979703
mayrars <mayra203@gmail.com>
2024-09-17 06:00:10 -0600

    import firebase and create register user

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,18 +1,20 @@
 import { defineStore } from "pinia";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
-        userData: 'ejemplo@test.com'
+        userData: null
     }),
-    getters:{
-        minuscula(state){
-            return state.userData.toLowerCase()
-        }
-    },
-    actions:{
-        registerUser(name){
-            this.userData = name
+    actions: {
+        async registerUser(email, password){
+            try{
+                const {user} = await createUserWithEmailAndPassword(auth, email, password)
+                this.userData = {email: user.email, uid: user.uid}
+            }catch(err){
+                console.log(err)
+            }
         }
     }
 })
