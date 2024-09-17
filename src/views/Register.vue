d0cdc745cae0595fede7cb7580c83c631637eec4
mayrars <mayra203@gmail.com>
2024-09-17 06:00:26 -0600

    create register user

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
         <h1>Register</h1>
-        <button @click="userStore.registerUser('Nacho')">Ingresar</button>
+        <form @submit.prevent="handleSubmit">
+            <input type="email" name="email" placeholder="Ingrese email" v-model.trim="email">
+            <input type="password" name="password" placeholder="Ingrese contraseña" v-model.trim="password">
+            <button type="submit">Crear usuario</button>
+        </form>
     </div>
 </template>
 
 <script setup>
-import { useUserStore } from '../stores/user';
-const userStore = useUserStore();
+    import {ref} from 'vue'
+    import { useUserStore } from '../stores/user'; 
 
+    const userStore = useUserStore()
+
+    const email = ref('')
+    const password = ref('')
+    const handleSubmit = () => {
+        if(!email.value || password.value.length<6) {
+            return alert('Campos vacios')
+        }
+        userStore.registerUser(email.value, password.value)
+    }
 </script>
