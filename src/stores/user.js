04783b32cb20dffc83c28a4fd3f63a69f0225dc1
mayrars <mayra203@gmail.com>
2024-09-18 05:54:33 -0600

    created loginUser

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 
 
@@ -14,6 +14,14 @@ export const useUserStore = defineStore('userStore', {
                 this.userData = {email: user.email, uid: user.uid}
             }catch(err){
                 console.log(err)
+            }
+        },
+        async loginUser(email, password){
+            try {
+                const {user} = await signInWithEmailAndPassword(auth, email, password)
+                this.userData = {email: user.email, uid: user.uid}
+            } catch (error) {
+                
             }
         }
     }
