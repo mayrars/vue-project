aa41b2520178ed67dc26c6dbb8e2fbd1e933eff0
mayrars <mayra203@gmail.com>
2024-09-18 05:55:10 -0600

    Created login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,29 @@
 <template>
     <div>
         <h1>Login</h1>
+        <form @submit.prevent="handleSubmit">
+            <input type="email" name="email" placeholder="Ingrese email" v-model.trim="email">
+            <input type="password" name="password" placeholder="Ingrese contraseña" v-model.trim="password">
+            <button type="submit">Acceso</button>
+        </form>
     </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue'
+    import { useUserStore } from '../stores/user'; 
+    import { useRouter } from 'vue-router'
+
+    const userStore = useUserStore()
+    const router = useRouter()
+
+    const email = ref('')
+    const password = ref('')
+    const handleSubmit = async() => {
+        if(!email.value || password.value.length<6) {
+            return alert('Campos vacios')
+        }
+        await userStore.loginUser(email.value, password.value)
+        router.push('/')
+    }
 </script>
