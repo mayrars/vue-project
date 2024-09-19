2ccc3058d3f68ea62552820808a8099ee6069833
mayrars <mayra203@gmail.com>
2024-09-19 06:03:37 -0600

    created require auth

--- src/router.js
+++ src/router.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "./stores/user";
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 
+const requireAuth = async (to, from, next) => {
+  const userStore = useUserStore();
+  userStore.loading = true;
+  const user = await userStore.currentUser();
+  if (user) {
+      next();
+  } else {
+      next("/login");
+  }
+  userStore.loading = false;
+};
+
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: Home, beforeEnter: requireAuth },
   { path: "/login", component: Login },
   { path: "/register", component: Register }
 ]
