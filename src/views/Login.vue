99aa886745bf30bc6416683edabb046f25b32496
mayrars <mayra203@gmail.com>
2024-09-19 06:02:35 -0600

    removing router

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,7 +4,7 @@
         <form @submit.prevent="handleSubmit">
             <input type="email" name="email" placeholder="Ingrese email" v-model.trim="email">
             <input type="password" name="password" placeholder="Ingrese contraseña" v-model.trim="password">
-            <button type="submit">Acceso</button>
+            <button type="submit" :disabled="userStore.loadingUser">Acceso</button>
         </form>
     </div>
 </template>
@@ -12,10 +12,8 @@
 <script setup>
     import { ref } from 'vue'
     import { useUserStore } from '../stores/user'; 
-    import { useRouter } from 'vue-router'
 
     const userStore = useUserStore()
-    const router = useRouter()
 
     const email = ref('')
     const password = ref('')
@@ -24,6 +22,5 @@
             return alert('Campos vacios')
         }
         await userStore.loginUser(email.value, password.value)
-        router.push('/')
     }
 </script>
