38de2f1ee9bd3c65454a92d6dd65c03f10481638
mayrars <mayra203@gmail.com>
2024-09-19 06:03:19 -0600

    adding router here and create logout and current user functions

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,28 +1,64 @@
 import { defineStore } from "pinia";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";
-
+import router from '../router';
 
 export const useUserStore = defineStore('userStore', {
     state: () => ({
-        userData: null
+        userData: null,
+        loadingUser: false
     }),
     actions: {
         async registerUser(email, password){
+            this.loadingUser = true
             try{
                 const {user} = await createUserWithEmailAndPassword(auth, email, password)
                 this.userData = {email: user.email, uid: user.uid}
+                router.push('/')
             }catch(err){
                 console.log(err)
+            } finally {
+                this.loadingUser = false
             }
         },
         async loginUser(email, password){
+            this.loadingUser = true
             try {
                 const {user} = await signInWithEmailAndPassword(auth, email, password)
                 this.userData = {email: user.email, uid: user.uid}
+                router.push('/')
             } catch (error) {
-                
+                console.log(err)                
+            }finally {
+                this.loadingUser = false
             }
-        }
+        },
+        async logOutUser(){
+            try {
+                await signOut(auth)
+                this.userData = null
+                router.push('/login')
+            } catch (error) {
+                console.log(err)                   
+            }
+        },
+        currentUser() {
+            return new Promise((resolve, reject) => {
+                const unsubcribe = onAuthStateChanged(
+                    auth,
+                    (user) => {
+                        if (user) {
+                            this.userData = {
+                                email: user.email,
+                                uid: user.uid,
+                            };
+                        }
+                        resolve(user);
+                    },
+                    (e) => reject(e)
+                );
+                unsubcribe();
+            });
+        },
     }
 })
