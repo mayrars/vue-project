529c8012da139566c21231dd974347c6a5ab1faf
mayrars <mayra203@gmail.com>
2024-09-19 06:03:59 -0600

    created button logout

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   <div>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> | º 
+      <router-link to="/login">Login</router-link> | 
       <router-link to="/register">Register</router-link> 
+      <button @click="userStore.logOutUser">Logout</button>
     </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script setup>
+  import { useUserStore } from './stores/user';
+  const userStore = useUserStore();
 
 </script>
