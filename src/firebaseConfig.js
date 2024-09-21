bfa79d5744b13bccd2dfc58add046c46b9d8adea
mayrars <mayra203@gmail.com>
2024-09-21 08:37:19 -0600

    adding db with firestore configuration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore/lite";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
