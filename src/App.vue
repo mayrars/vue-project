c969b2e3df0c51d9748f1e0bf9353822ecf8ae9f
mayrars <mayra203@gmail.com>
2024-09-21 08:37:01 -0600

    Adding loading

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <nav v-if="!userStore.loadingSession">
-      <router-link to="/" v-if="userStore.userData">Home</router-link> |
-      <router-link to="/login" v-if="!userStore.userData">Login</router-link> | 
-      <router-link to="/register" v-if="!userStore.userData">Register</router-link> 
-      <button @click="userStore.logOutUser" v-if="userStore.userData">Logout</button>
-    </nav>
-    <div v-else>Loading...</div>
+    <div v-if="userStore.loading">loading...</div>
+    <div v-else="!userStore.loading">
+      <nav >
+        <router-link to="/" v-if="userStore.userData">Home</router-link> |
+        <router-link to="/login" v-if="!userStore.userData">Login</router-link> | 
+        <router-link to="/register" v-if="!userStore.userData">Register</router-link> 
+        <button @click="userStore.logOutUser" v-if="userStore.userData">Logout</button>
+      </nav>
+    </div>
     <router-view></router-view>
   </div>
 </template>
