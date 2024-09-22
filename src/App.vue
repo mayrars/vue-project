0fbc772916ed2a585fac8c208600101e0cfa5ac8
mayrars <mayra203@gmail.com>
2024-09-22 17:15:38 -0600

    change loading message

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <div v-if="userStore.loading">loading...</div>
-    <div v-else="!userStore.loading">
+    <div v-if="!userStore.loadingSession">
       <nav >
         <router-link to="/" v-if="userStore.userData">Home</router-link> |
         <router-link to="/login" v-if="!userStore.userData">Login</router-link> | 
         <router-link to="/register" v-if="!userStore.userData">Register</router-link> 
         <button @click="userStore.logOutUser" v-if="userStore.userData">Logout</button>
       </nav>
+    </div>
+    <div v-else>
+      <p>Loading...</p>
     </div>
     <router-view></router-view>
   </div>
