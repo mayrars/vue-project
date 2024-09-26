cb3684ddfc9bba75004c16295be4dfdc03643408
mayrars <mayra203@gmail.com>
2024-09-26 05:48:33 -0600

    Created Edit view

---
+++ src/views/Editar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Editar</h1>
+        <form @submit.prevent="handleSubmit">
+            <input type="text" placeholder="Ingrese la URL" v-model="url">
+            <button type="submit">Editar</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { useDatabaseStore } from '../stores/database';
+const route = useRoute();
+const databaseStore = useDatabaseStore();
+
+const handleSubmit= ()=> {
+    
+}
+
+const url = ref('')
+onMounted(async()=>{
+    url.value = await databaseStore.readUrl(route.params.id)
+})
+</script>
