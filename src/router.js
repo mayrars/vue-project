2db71e7f587546f4de6812edb4887379e0e431ea
mayrars <mayra203@gmail.com>
2024-09-26 05:49:00 -0600

    Created edit route

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { useUserStore } from "./stores/user";
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
+import Editar from "./views/Editar.vue";
 
 const requireAuth =  async (to, from, next) => {
   const userStore = useUserStore();
@@ -18,6 +19,7 @@ const requireAuth =  async (to, from, next) => {
 
 const routes = [
   { path: "/", component: Home, beforeEnter: [requireAuth] },
+  { path: "/edit/:id", component: Editar, beforeEnter: [requireAuth]},
   { path: "/login", component: Login },
   { path: "/register", component: Register }
 ]
