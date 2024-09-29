b2dceb2490fa9c053f656b616c857568e16e8afc
mayrars <mayra203@gmail.com>
2024-09-29 14:13:02 -0600

    Cambiando el boton

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div v-if="!userStore.loadingSession">
+      <a-button type="primary">Primary</a-button>
       <nav >
         <router-link to="/" v-if="userStore.userData">Home</router-link> |
         <router-link to="/login" v-if="!userStore.userData">Login</router-link> | 
