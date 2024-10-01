60da2d0148c704604d9ac30b457db7312ef5dd5f
mayrars <mayra203@gmail.com>
2024-10-01 08:16:25 -0600

    Change template for ant design modules and adding route and watch

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
-  <div>
-    <div v-if="!userStore.loadingSession">
-      <a-button type="primary">Primary</a-button>
+  <a-layout>
+    <a-layout-header v-if="!userStore.loadingSession">
+      <a-menu 
+        theme="dark" 
+        mode="horizontal" 
+        :style="{ lineHeight: '64px'}" 
+        v-model:selectedKeys="selectedKeys"
+      >
+        <a-menu-item key="home" v-if="userStore.userData">
+          <router-link to="/" >Home</router-link>
+        </a-menu-item>
+        <a-menu-item key="login" v-if="!userStore.userData">
+          <router-link to="/login" >Login</router-link>
+        </a-menu-item>
+        <a-menu-item key="register" v-if="!userStore.userData">
+          <router-link to="/register">Register</router-link> 
+        </a-menu-item>
+        <a-menu-item key="logout" @click="userStore.logOutUser" v-if="userStore.userData">
+          Logout
+        </a-menu-item>
+      </a-menu>
       <nav >
-        <router-link to="/" v-if="userStore.userData">Home</router-link> |
-        <router-link to="/login" v-if="!userStore.userData">Login</router-link> | 
-        <router-link to="/register" v-if="!userStore.userData">Register</router-link> 
-        <button @click="userStore.logOutUser" v-if="userStore.userData">Logout</button>
+        
+        
+        
+        
       </nav>
-    </div>
-    <div v-else>
-      <p>Loading...</p>
-    </div>
-    <router-view></router-view>
-  </div>
+    </a-layout-header>
+    <a-layout-content style="padding: 0 50px">
+      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">
+        <div v-if="userStore.loadingSession">Loading...</div>
+        <router-view></router-view>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script setup>
+  import { ref, watch } from 'vue';
   import { useUserStore } from './stores/user';
-  const userStore = useUserStore();
+  import { useRoute } from 'vue-router'
 
+  const userStore = useUserStore();
+  const route = useRoute()
+  const selectedKeys = ref(['login']);
+  watch(
+    ()=>route.name,
+    ()=>{
+      selectedKeys.value = route.name
+    }
+  )
 </script>
