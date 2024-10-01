873ba12050e9f29c7df1cfa8e358b498c500d427
mayrars <mayra203@gmail.com>
2024-10-01 08:52:10 -0600

    Change form for ant design form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <form @submit.prevent="handleSubmit">
-            <input type="email" name="email" placeholder="Ingrese email" v-model.trim="email" />
-            <input type="password" placeholder="Ingrese contraseña" v-model.trim="password" />
-            <button type="submit" :disabled="userStore.loadingUser">Acceso</button>
-        </form>
-    </div>
+    <a-row>
+        <a-col :xs="{span:24}" :sm="{span:12, offset:6}" :md="{span:12, offset:6}" :lg="{span:12, offset:6}" :xl="{span:12, offset:6}">
+            <h1>Login</h1>
+            <a-form 
+                name="basicLogin" 
+                autocomplete="off" 
+                layout="vertical"
+                @submit.prevent="handleSubmit"
+            >
+                <a-form-item 
+                    name="email" 
+                    label="Ingresa tu correo" 
+                    :rules="[{ required: true, message: 'Ingresa tu correo' }]"
+                >
+                    <a-input v-model:value="email"></a-input>
+                </a-form-item>
+                <a-form-item
+                    label="Ingrese contraseña"
+                    name="password"
+                    :rules="[{ required: true, message: 'Ingresa una contraseña' }]"
+                >
+                    <a-input-password v-model:value="password" />
+                </a-form-item>
+                <a-form-item>
+                    <a-button type="primary" html-type="submit" :disabled="userStore.loadingUser">Acceso</a-button>
+                </a-form-item>
+            </a-form>
+        </a-col>    
+    </a-row>
 </template>
 
 <script setup>
