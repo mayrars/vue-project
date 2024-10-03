43363674985b56097f0c68aef27a5a2c42e7e4c4
mayrars <mayra203@gmail.com>
2024-10-03 05:46:32 -0600

    Finish Register form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,58 +1,96 @@
 <template>
     <a-row>
-        <a-col :xs="{span:24}" :sm="{span:12, offset:6}" :md="{span:12, offset:6}" :lg="{span:12, offset:6}" :xl="{span:12, offset:6}">
-            <h1>Register</h1>
-
-            <a-form-item 
-                name="email"
-                label="Ingrese email"
+        <a-col
+            :xs="{ span: 24 }"
+            :sm="{ span: 18, offset: 3 }"
+            :lg="{ span: 12, offset: 6 }"
+        >
+            <a-form
+                :model="formState"
+                @finishFailed="onFinishFailed"
+                @finish="onFinish"
+                name="basicTwo"
+                layout="vertical"
+                autocomplete="off"
             >
-                <a-input v-model:value="formState.email" placeholder="" />
-            </a-form-item>
-            <a-form-item 
-                name="password" 
-                label="Ingrese contraseña" 
-                :rules="[
-                    { 
-                        required: true, 
-                        min: 6,
-                        whiteSpace: true,
-                        message: 'Ingresa una contraseña con minimo 6 caracteres' 
-                    }
-                ]"
-            >
-                <a-input-password v-model:value="formState.password"/>
-            </a-form-item>
-            <a-form-item
-                name="repassword"
-                label="Repita la contraseña"
-                :rules="[
-                    {
-                        required: true, 
-                        min: 6,
-                        whiteSpace: true
-                    }
-                ]"  
-            >
-                <a-input-password v-model:value="formState.repassword"/>
-            </a-form-item>
+                <h1>Register</h1>
+                <a-form-item
+                    label="Email"
+                    name="email"
+                    :rules="[
+                        {
+                            required: true,
+                            type: 'email',
+                            message: 'Por favor escriba un email válido',
+                        },
+                    ]"
+                >
+                    <a-input v-model:value="formState.email"></a-input>
+                </a-form-item>
+                <a-form-item
+                    label="Password"
+                    name="password"
+                    :rules="[
+                        {
+                            required: true,
+                            min: 6,
+                            message:
+                                'Por favor escriba una contraseña de 6 carácteres',
+                        },
+                    ]"
+                >
+                    <a-input-password
+                        v-model:value="formState.password"
+                    ></a-input-password>
+                </a-form-item>
+                <a-form-item
+                    label="Repita Password"
+                    name="repassword"
+                    :rules="{ validator: validateRePass }"
+                >
+                    <a-input-password
+                        v-model:value="formState.repassword"
+                    ></a-input-password>
+                </a-form-item>
+                <a-form-item>
+                    <a-button type="primary" html-type="submit"
+                        >Acceder</a-button
+                    >
+                </a-form-item>
+            </a-form>
         </a-col>
     </a-row>
 </template>
 
 <script setup>
-    import { reactive } from 'vue'
-    import { useUserStore } from '../stores/user';
+import { reactive } from "vue";
+import { useUserStore } from "../stores/user";
 
-    const userStore = useUserStore()
-    const formState = reactive({
-        email: '',
-        password: ''
-    })
-    const handleSubmit = async() => {
-        if(!email.value || password.value.length<6) {
-            return alert('Campos vacios')
-        }
-        await userStore.registerUser(email.value, password.value)
+const userStore = useUserStore();
+
+const formState = reactive({
+    password: "",
+    repassword: "",
+    email: "",
+});
+
+const validateRePass = async (_rule, value) => {
+    if (value === "") {
+        return Promise.reject("Por favor repita contraseña");
     }
+    if (value !== formState.password) {
+        return Promise.reject("No coinciden las contraseñas");
+    }
+    Promise.resolve();
+};
+
+const onFinish = async (values) => {
+    console.log("Success:", values);
+    await userStore.registerUser(values.email, values.password);
+    alert("Verifica tu cuenta de correo")
+};
+
+const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+};
 </script>
