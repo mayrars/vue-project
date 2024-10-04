cb5cd51a55faabe4e13a37c6b12b2b8d9f13ce6d
mayrars <mayra203@gmail.com>
2024-10-04 07:05:44 -0600

    Created addForm

---
+++ src/components/AddForm.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { reactive } from 'vue'
+    const formState = reactive({
+        url: ''
+    })
+    const onFinish = async(values) => {
+        console.log('Success:', values);
+    }
+</script>
+<template>
+    <a-form 
+        name="addForm"
+        autocomplete="off"
+        layout="vertical"
+        :model="formState"
+        @finish="onFinish"
+    >
+        <a-form-item
+            name="url"
+            label="ingrese una URL"
+            :rules="[
+                {
+                    required: true,
+                    whitespace: true,
+                    pattern=/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
+                    message: 'Por favor ingrese una URL correcta'
+                }
+            ]"
+        ></a-form-item>
+        <a-form-item></a-form-item>
+    </a-form>
+</template>
